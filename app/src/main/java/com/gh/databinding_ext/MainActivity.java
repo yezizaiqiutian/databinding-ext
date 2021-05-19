b0c6d72ee0426924c11ac806597ab75e45426182
gaohui01 <gaohui01@5i5j.com>
2021-05-19 15:01:02 +0800

    主项目提交

---
+++ app/src/main/java/com/gh/databinding_ext/MainActivity.java
@@ -0,0 +1,12 @@
+package com.gh.databinding_ext;
+
+import android.app.Activity;
+
+/**
+ * @author: gh
+ * @description:
+ * @date: 5/19/21.
+ * @from:
+ */
+public class MainActivity extends Activity {
+}
